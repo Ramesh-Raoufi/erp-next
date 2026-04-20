896bf1e4111a97bad21dfa91f39032ca8b10736f
Ahmad Ramesh Raoufi <ramesh.raoufi@gmail.com>
2026-04-20 08:04:25 +0000

    fix: login card inline width 360px to force correct size

--- page-components/LoginPage.tsx
+++ page-components/LoginPage.tsx
@@ -40,7 +40,7 @@ export function LoginPage() {
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-50 p-4">
-      <div className="w-[360px] space-y-6">
+      <div style={{width: '360px'}} className="space-y-6">
 
         {/* Header */}
         <div className="text-center">
